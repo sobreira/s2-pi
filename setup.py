4f0a185123a72ca3cee33f7d1f426bf715222787
Edson Sidnei Sobreira <5200448+sobreira@users.noreply.github.com>
2018-11-16 23:55:44 -0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ call(["wget", "-P", pth, "https://raw.githubusercontent.com/MrYsLab/s2-pi/master
 
 setup(
     name='s2-pi',
-    version='1.22',
+    version='1.23',
     packages=['s2_pi'],
 
     entry_points={
