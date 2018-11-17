0fe92fc052d8b611ea60682b9d5b8c1b2f205372
Edson Sidnei Sobreira <5200448+sobreira@users.noreply.github.com>
2018-11-16 23:06:25 -0200

    Update s2_pi/s2_pi.py

--- s2_pi/s2_pi.py
+++ s2_pi/s2_pi.py
@@ -98,8 +98,8 @@ class S2Pi(WebSocket):
             # when a user wishes to output MCP3008
         elif client_cmd == 'mcp_3008':
             spi = spidev.SpiDev() # Created an object
-            spi.open(0,0)	
-            pin = int([payload['pin'])
+            spi.open(0,0)
+            pin = int(payload['pin'])
             #pin = int((payload['pin'])[-1])
             
             spi.max_speed_hz = 1350000
